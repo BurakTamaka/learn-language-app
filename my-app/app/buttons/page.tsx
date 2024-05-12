33480284521c63494c840bdfa0b57692feb7057c
BurakTamaka <buraktamaka00@gmail.com>
2024-05-12 22:12:01 +0300

    first Commit

---
+++ my-app/app/buttons/page.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () =>{
+    return(
+        <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+            <Button variant="default">
+                Default
+            </Button>
+            <Button variant="primary">
+                primary
+            </Button>
+            <Button variant="primaryOutline">
+                Primary Outline
+            </Button>
+            <Button variant="secondary">
+                Secondary
+            </Button>
+            <Button variant="secondaryOutline">
+            Secondary Outline
+            </Button>
+            <Button variant="danger">
+                danger
+            </Button>
+            <Button variant="dangerOutline">
+                danger Outline
+            </Button>
+            <Button variant="super">
+                super
+            </Button>
+            <Button variant="superOutline">
+                super Outline
+            </Button>
+            <Button variant="ghost">
+                ghost
+            </Button>
+            <Button variant="sidebar">
+                sidebar
+            </Button>
+            <Button variant="sidebarOutline">
+                sidebar Outline
+            </Button>
+
+        </div>
+
+    );
+}
+
+export default ButtonsPage;
